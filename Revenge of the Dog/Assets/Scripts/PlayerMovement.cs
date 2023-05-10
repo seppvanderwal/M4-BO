5c7a90d23ee310fb81ae8df767691844fa656ce4
Gino <35615@ma-web.nl>
2023-05-10 12:14:55 +0200

    movement script with jumping mechanic

--- Revenge of the Dog/Assets/Scripts/PlayerMovement.cs
+++ Revenge of the Dog/Assets/Scripts/PlayerMovement.cs
@@ -4,78 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5f;
+    private float direction = 0f;
+    
     private Rigidbody2D rb;
-    private BoxCollider2D coll;
-    private SpriteRenderer sprite;
-    private Animator anim;
 
-    [SerializeField] private LayerMask jumpableGround;
 
-    private float dirX = 0f;
-    [SerializeField] private float moveSpeed = 7f;
-    [SerializeField] private float jumpForce = 14f;
-
-    private enum MovementState { idle, running, jumping, falling }
-
-    [SerializeField] private AudioSource jumpSoundEffect;
-
-    // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        coll = GetComponent<BoxCollider2D>();
-        sprite = GetComponent<SpriteRenderer>();
-        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        direction = Input.GetAxis("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (direction > 0f)
         {
-            jumpSoundEffect.Play();
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = new Vector2(direction * speed, rb.velocity.y);
         }
-
-        UpdateAnimationState();
-    }
-
-    private void UpdateAnimationState()
-    {
-        MovementState state;
-
-        if (dirX > 0f)
+        else if (direction < 0f)
         {
-            state = MovementState.running;
-            sprite.flipX = false;
-        }
-        else if (dirX < 0f)
-        {
-            state = MovementState.running;
-            sprite.flipX = true;
+            rb.velocity = new Vector2(direction * speed, rb.velocity.y);
         }
         else
         {
-            state = MovementState.idle;
-        }
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }   
 
-        if (rb.velocity.y > .1f)
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            state = MovementState.jumping;
-        }
-        else if (rb.velocity.y < -.1f)
-        {
-            state = MovementState.falling;
+            float force = 5f;
+            rb.velocity = Vector2.up * force;
         }
 
-        anim.SetInteger("state", (int)state);
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
