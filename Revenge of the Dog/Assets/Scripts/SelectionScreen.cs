30953257ff2de93e07ec6b983a0b9253b58842bc
Gino <35615@ma-web.nl>
2023-05-11 11:16:24 +0200

    startscreen, game over screen, level selection screen has been finished and is getting the shop update later

---
+++ Revenge of the Dog/Assets/Scripts/SelectionScreen.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(2);
+        //A.Stop();// stopt het muziek
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(3);
+        //A.Stop();// stopt het muziek
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(4);
+        //A.Stop();// stopt het muziek
+    }
+    public void Level4()
+    {
+        SceneManager.LoadScene(5);
+        //A.Stop();// stopt het muziek
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+        //A.Stop();// stopt het muziek
+    }
+
+}
