3690510475d8834ddcdb07921c8c9f97a8695d6a
Gino <35615@ma-web.nl>
2023-05-16 09:44:03 +0200

    fixes were made

---
+++ Revenge of the Dog/Assets/Scripts/SelectionScreen.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(2);
+        //A.Stop();// stopt het muziek
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(3);
+        //A.Stop();// stopt het muziek
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(4);
+        //A.Stop();// stopt het muziek
+    }
+    public void Level4()
+    {
+        SceneManager.LoadScene(5);
+        //A.Stop();// stopt het muziek
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+        //A.Stop();// stopt het muziek
+    }
+
+    public void Shop()
+    {
+        SceneManager.LoadScene(1);
+        //A.Stop();// stopt het muziek
+    }
+
+}
