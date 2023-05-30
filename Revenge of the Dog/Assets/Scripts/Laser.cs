afb6119d5d06d840c165cd5a5fcf79c3ce114c67
Sepp <34765@ma-web.nl>
2023-05-30 09:15:19 +0200

    laser

--- Revenge of the Dog/Assets/Scripts/Laser.cs
+++ Revenge of the Dog/Assets/Scripts/Laser.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using UnityEngine;
 
@@ -13,20 +14,26 @@ public class Laser : MonoBehaviour
     public Transform firePoint;
     public GameObject StartVFX;
     public GameObject EndVFX;
+    public float cooldown;
+    public float lastShot;
 
     private List<ParticleSystem> particles = new List<ParticleSystem>();
+    private bool canShoot = true;
 
     void Start()
-    {   
+    {
+        
         FillLists();
         DisableLaser();
+        canShoot = true;
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && canShoot == true)
         {
             EnableLaser();
+           
         }
 
         if (Input.GetButton("Fire1"))
@@ -36,14 +43,28 @@ public class Laser : MonoBehaviour
 
         if (Input.GetButtonUp("Fire1"))
         {
+            canShoot= false;
             DisableLaser();
+            
         }
 
         
 
     }
+ 
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(1f);
+        canShoot = true;
+    }
+    
         void EnableLaser()
         {
+        if (Time.time - lastShot < cooldown)
+        {
+            return;
+        }
+        lastShot = Time.time;
             lineRenderer.enabled = true;
 
         for (int i = 0; i < particles.Count; i++)
@@ -76,7 +97,8 @@ public class Laser : MonoBehaviour
 
         for (int i = 0; i < particles.Count; i++)
             particles[i].Stop();
-    }
+            StartCoroutine(Cooldown());
+        }
 
         void FillLists()
         {
